8a2fbcdf4ff211b09103e55bf20451c4834fceae
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-12 09:54:30 -0500

    seed files

--- data/seeds/001_tasks.js
+++ data/seeds/001_tasks.js
@@ -1,13 +1,34 @@
+const tasks = [
+  {
+    name: "noodles"
+  },
+  {
+    name: "eggs"
+  },
+  {
+    name: "broccoli"
+  },
+  {
+    name: "push-ups"
+  },
+  {
+    name: "cardio"
+  },
+  {
+    name: "finish lambda project"
+  },
+  {
+    name: "give my dog a bath"
+  },
+  {
+    name: "buy groceries"
+  }
+];
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex("tasks").then(function() {
+    // Inserts seed entries
+    return knex("tasks").insert(tasks);
+  });
 };
