d06aa9b0467b455c2f6989ba31cb5f5623425989
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-12 06:43:54 -0500

    knex setup for postgres

--- knexfile.js
+++ knexfile.js
@@ -4,25 +4,34 @@ module.exports = {
 
   development: {
     client: 'pg',
+    useNullableAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      database: process.env.DB_DEV_DATABASE,
+      user: process.env.DB_DEV_USER,
+      password: process.env.DB_DEV_PASSWORD
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   },
 
   
   production: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds "
     }
   }
 
