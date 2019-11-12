507c6ff89547b67819d0108668387666d2b80602
Robert Benavidez <murabitbenavidez@gmail.com>
2019-11-12 06:31:30 -0500

    changes

--- knexfile.js
+++ knexfile.js
@@ -3,30 +3,15 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'pg',
     connection: {
       filename: './dev.sqlite3'
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
+  
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       database: 'my_db',
       user:     'username',
